5f2648149422dfcdfc3579dc4749a62aa10183e2
Entity-now <2966388213@qq.com>
2024-07-04 21:43:33 +0800

    更新音频流

--- Edge_tts_sharp/Utils/Audio.cs
+++ Edge_tts_sharp/Utils/Audio.cs
@@ -11,7 +11,7 @@ using NAudio.Wave;
 
 namespace Edge_tts_sharp.Utils
 {
-    public class Audio
+    public static class Audio
     {
         public static async Task PlayToStreamAsync(Stream source, float volume = 1.0f)
         {
@@ -68,5 +68,76 @@ namespace Edge_tts_sharp.Utils
                 await PlayToStreamAsync(stream, volume);
             }
         }
+
+        public static async Task PlayNetworkAudio(this HttpClient client, string url, float volume = 1.0f)
+        {
+            try
+            {
+                using (var responseStream = await client.GetStreamAsync(url))
+                {
+                    var bufferedWaveProvider = new BufferedWaveProvider(new WaveFormat());
+                    using (var waveOut = new WaveOutEvent())
+                    {
+                        waveOut.Init(bufferedWaveProvider);
+                        waveOut.Volume = volume;
+                        waveOut.Play();
+
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+                        while ((bytesRead = await responseStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                        {
+                            bufferedWaveProvider.AddSamples(buffer, 0, bytesRead);
+                            // 调整这个延迟时间以确保缓冲区有足够的数据播放
+                            await Task.Delay(10);
+                        }
+
+                        // 等待播放完成
+                        while (waveOut.PlaybackState == PlaybackState.Playing && bufferedWaveProvider.BufferedDuration.TotalMilliseconds > 0)
+                        {
+                            await Task.Delay(50);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error playing network audio: {ex.Message}");
+            }
+        }
+        public static async Task PlayNetworkAudio(this HttpClient client, string url, float volume = 1.0f, int bufferSize = 4096, int delay = 10, Action callBack = null)
+        {
+            try
+            {
+                using (var responseStream = await client.GetStreamAsync(url))
+                {
+                    var bufferedWaveProvider = new BufferedWaveProvider(new WaveFormat());
+                    using (var waveOut = new WaveOutEvent())
+                    {
+                        waveOut.Init(bufferedWaveProvider);
+                        waveOut.Volume = volume;
+                        waveOut.Play();
+
+                        byte[] buffer = new byte[bufferSize];
+                        int bytesRead;
+                        while ((bytesRead = await responseStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                        {
+                            bufferedWaveProvider.AddSamples(buffer, 0, bytesRead);
+                            await Task.Delay(delay);
+                        }
+
+                        waveOut.PlaybackStopped += (s, e) =>
+                        {
+                            // 处理播放停止的情况
+                            callBack();
+                        };
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error playing network audio: {ex.Message}");
+            }
+        }
+
     }
 }
